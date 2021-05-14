3823e79a0ac3da33aa304fb92579862350025a8f
Yemi Shin <shiny@carleton.edu>
2021-05-13 21:09:21 -0500

    adding another type of memory

--- component6/guess_who.py
+++ component6/guess_who.py
@@ -68,7 +68,7 @@ class GuessWho:
     General-Response -> I think it was <Person to be Blamed> who did it. <Scared>.
     Person to be Blames -> ...
     '''
-    memory_type_list = ["Alibi", "Action", "Residence", "Relationship", "Name",  "Company", "Memory with Victim", "Person to be Blamed", "Location"]
+    memory_type_list = ["Alibi", "Action", "Residence", "Relationship", "Name", "Name Of Victim", "Company", "Memory with Victim", "Person to be Blamed", "Location"]
     grammar_engine = GrammarEngine("component6/grammar/keyphrases_trigger.txt")
     for item in self.suspect_memory:
       if item.type_of_memory in memory_type_list:
